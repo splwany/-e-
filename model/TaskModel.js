55bb8085817863bf18981c6df93a10f8f25bfd3b
Gavid <460528401@qq.com>
2019-10-18 16:42:34 +0800

    #commit

--- model/TaskModel.js
+++ model/TaskModel.js
@@ -1,11 +1,37 @@
-export default {
+import RequestUrls from "../utils/RequestUrls"
+import HelperUtil from "../utils/HelperUtil"
 
-  addTask () {
+// 属性
+let taskModel = {
+ 
+};
 
-  },
-
-  setTaskTimeLimit (taskID, timeLimit) {
-
-  }
+// 行为方法
+class taskModelImpl{
   
 }
+
+// 静态方法
+let staticMethods = {
+  // 创建任务对象
+  createTaskModel: function () {
+    return Object.freeze(Object.assign({}, taskModel));
+  },
+
+  // 添加任务
+  addApplyFromModel: function(taskModel){
+    return HelperUtil.httpReq(RequestUrls.TASK_CRUD,taskModel);
+  }
+};
+
+// 封装对象
+function TaskModel(){
+  return Object.freeze(Object.assign({
+        // 在此列出模型对象方法
+      },
+      staticMethods // 将静态模型方法注入到模型对象方法中
+  ));
+}
+
+Object.assign(TaskModel,staticMethods);
+export default TaskModel;
