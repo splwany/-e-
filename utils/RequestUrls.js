2062d761d4117f26aa29c31e1d48c5bff81458b6
Gavid <460528401@qq.com>
2019-10-18 16:36:59 +0800

    #commit

--- utils/RequestUrls.js
+++ utils/RequestUrls.js
@@ -1,3 +1,14 @@
 const ROOT_URL = "";
-// 用电申请单 URL
-export const APPLYFORM_CRUD = ROOT_URL + "";
+var urls = {
+  // 用电申请单 URL
+  'APPLYFORM_CRUD': ROOT_URL + "",
+  // 任务 URL
+  'TASK_CRUD': ROOT_URL + "",
+};
+
+function RequestUrls(){
+  return Object.freeze(Object.assign({},urls));
+}
+
+Object.assign(RequestUrls,urls);
+export default RequestUrls
